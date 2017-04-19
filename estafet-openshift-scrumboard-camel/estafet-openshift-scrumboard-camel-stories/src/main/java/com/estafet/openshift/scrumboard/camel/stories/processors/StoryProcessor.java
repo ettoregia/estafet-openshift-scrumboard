639e3b8054d37258578366f15a879841b416f0c8
Ettore Giallaurito <ettore.giallaurito@estafet.com>
2017-04-19 11:02:06 +0100

    Added DAO dependencies into middleware and rewritten the processor with the right methods

--- estafet-openshift-scrumboard-camel/estafet-openshift-scrumboard-camel-stories/src/main/java/com/estafet/openshift/scrumboard/camel/stories/processors/StoryProcessor.java
+++ estafet-openshift-scrumboard-camel/estafet-openshift-scrumboard-camel-stories/src/main/java/com/estafet/openshift/scrumboard/camel/stories/processors/StoryProcessor.java
@@ -1,36 +1,40 @@
 package com.estafet.openshift.scrumboard.camel.stories.processors;
 
+import com.estafet.openshift.scrumboard.camel.stories.services.api.AddAcceptanceCriteriaParameters;
+import com.estafet.openshift.scrumboard.camel.stories.services.api.AssignStoryPointsParameters;
+import com.estafet.openshift.scrumboard.camel.stories.services.api.AssignTaskToStoryParameters;
+import com.estafet.openshift.scrumboard.camel.stories.services.api.ChangeStoryDetailsParameters;
+import com.estafet.openshift.scrumboard.dao.api.StoryDAO;
 import com.estafet.openshift.scrumboard.entity.Story;
 
 public class StoryProcessor {
-	
-	public void createStory(Story story) {
-        // get the id of the input
-        //ClaimInput input = exchange.getIn().getBody(ClaimInput.class);
-        System.out.println(story);
-    }
-	
+
+	private StoryDAO storyDAO;
+
+
+	public int createStory(Story story) {
+		return 0;
+	}
+
 	public void assignStorypointsToStoryById(Story story) {
-        // get the id of the input
-        //ClaimInput input = exchange.getIn().getBody(ClaimInput.class);
-        System.out.println(story);
-    }
-	
-	public void assignTaskToStory(Story story) {
-        // get the id of the input
-        //ClaimInput input = exchange.getIn().getBody(ClaimInput.class);
-        System.out.println(story);
-    }
-	
-	public void findStoryById(Story story) {
-        // get the id of the input
-        //ClaimInput input = exchange.getIn().getBody(ClaimInput.class);
-        System.out.println(story);
-    }
-	
-	public void deleteStoryById(Story story) {
-        // get the id of the input
-        //ClaimInput input = exchange.getIn().getBody(ClaimInput.class);
-        System.out.println(story);
-    }
+	}
+
+	public Story getStory(int storyId) {
+		return null;
+	}
+
+	public void assignStoryPoints(AssignStoryPointsParameters parameters) {
+	}
+
+	public void changeStoryDetails(ChangeStoryDetailsParameters parameters) {
+	}
+
+	public void deleteStory(int storyId) {
+	}
+
+	public void addAcceptanceCriteria(AddAcceptanceCriteriaParameters parameters){}
+
+	public void removeAcceptanceCriteria(int acceptanceCriteriaId){}
+
+	public void assignTaskToStory(AssignTaskToStoryParameters parameters){}
 }
